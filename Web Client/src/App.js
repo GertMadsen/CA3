96f294ef30594c062eae8e756af88d74ad77a814
GertMadsen <cph-gm50@cphbusiness.dk>
2018-05-03 11:43:21 +0200

    detail side lavet

--- Web Client/src/App.js
+++ Web Client/src/App.js
@@ -154,7 +154,7 @@ class ShowCars extends Component {
       <div class="row">
         <div class="col-sm-2"></div>
         <div class="col-sm-8">
-          <div class="well well-lg"> <h2> List of Cars</h2> </div>
+          <div class="well well-sm"> <h3> List of Cars</h3> </div>
           <table class="table" key="tableList">
             <tbody>
               <tr>               
@@ -179,6 +179,84 @@ class ShowCars extends Component {
     )
   }
 }
+
+
+
+
+
+
+class CarDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { fetchURL: props.fetchURL, dataFromServer: {}, regno: props.match.params.regno };
+  }
+  componentDidMount() { 
+    facade.fetchSingleCar(this.state.regno).then(res => this.setState({ dataFromServer: res }));
+  }
+  render() {
+    var car = this.state.dataFromServer;
+
+    return (
+      <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+          <div class="well well-sm"> <h3> Car Details</h3> </div>
+          <img src={car.logo} width="100px" height="100px" alt=""/>
+          <h2>{car.company}</h2>
+
+                    <table class="table" key="tableList">
+            <tbody>
+              <tr>               
+                <th scope="col">Category</th>
+                <th scope="col">Make</th>
+                <th scope="col">Model</th>
+                <th scope="col">Year</th>
+                <th scope="col">Regno</th>
+                <th scope="col">Seats</th>
+                <th scope="col">Doors</th>
+                <th scope="col">Gear</th>
+                <th scope="col">Aircondition</th>
+                <th scope="col">Location</th>
+                <th scope="col">PricePerDay</th>
+                <th scope="col">Booking</th>
+              </tr>
+              <tr  scope="row" key={car.regno}>
+              <td>{car.category}</td>
+              <td>{car.model}</td>         
+              <td>{car.make}</td>
+              <td>{car.year}</td>
+              <td>{car.regno}</td>
+              <td>{car.seats}</td>
+              <td>{car.doors}</td>
+              <td>{car.gear}</td>
+              <td>{""+car.aircondition}</td>
+              <td>{car.location}</td>
+              <td>{car.priceperday}</td>
+          {/* <td><Link to={`booking/${car.regno}`} class="btn btn-success">Book</Link></td> */}
+          <td><Link to='/details/{this.state.regno}' class="btn btn-success">Book</Link></td>
+        </tr>
+
+            </tbody>
+          </table>
+
+          <img src={car.picture} width="30%" height="30%" alt=""/>
+
+          <br /><br />
+
+
+
+          <Link to="/showcars" class="btn btn-success">Back</Link>
+
+        </div>
+        <div class="col-sm-2"></div>
+      </div>
+
+    )
+  }
+}
+
+
+
 
 
 class Header extends Component {
@@ -226,9 +304,6 @@ class App extends Component {
     this.setState({ fetchURL: url });
     console.log(url);
   }
-
-
-
   render() {
     return (
       <div>
@@ -239,6 +314,7 @@ class App extends Component {
             <Switch>
               <Route exact path="/" render={() => <RentCar setURL={this.setURL}/>} />
               <Route path="/showcars" render={() => <ShowCars fetchURL={this.state.fetchURL} />} />
+              <Route path="/details/:regno" render={(props) => <CarDetails {...props} />} />
               
               {/* <Route path="/swapi" render={() => <FetchSwapi />} /> */}
               {/* <Route path="/userdata" render={() => <UserData />} />
