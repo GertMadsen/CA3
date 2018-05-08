60093e1d944e471644dc6e82480595ef67f81e4e
Lene Skov <lsko@itu.dk>
2018-05-08 10:37:08 +0200

    oprettet bookingfacade

---
+++ Backend/src/main/java/entity/BookingFacade.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author lene_
+ */
+public class BookingFacade {
+    
+    public void createBooking(Car car, Customer customer){
+        
+    }
+    
+}
